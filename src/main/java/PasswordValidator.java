f33ad7491191efaf4f73c13a97e4e37e7bc08a83
Franck Colonna <contact@franckcolonna.fr>
2021-05-07 14:01:43 +0200

    Adding char test + other test

--- src/main/java/PasswordValidator.java
+++ src/main/java/PasswordValidator.java
@@ -3,16 +3,23 @@ public class PasswordValidator {
         if (isValidLenght(pwd))
                 return false;
         int nbDigit = 0;
+        int nbChar = 0;
         for (int i=0; i < pwd.length(); i++){
             char currentChar = pwd.charAt(i);
             if (Character.isDigit(currentChar)){
                 nbDigit++;
             }
+            if (Character.isLetter(currentChar)){
+                nbChar++;
+            }
         }
         if (nbDigit == 0) {
             return false;
+        }else if (nbChar == 0) {
+            return false;
+        }else {
+            return true;
         }
-        return true;
     }
 
     private static boolean isValidLenght(String pwd) {
--- src/test/java/PasswordValidatorTest.java
+++ src/test/java/PasswordValidatorTest.java
@@ -5,12 +5,32 @@ import static org.junit.jupiter.api.Assertions.*;
 class PasswordValidatorTest {
 
     @Test
-    public void minimalLenghtPasswordTest() {
-        assertTrue(PasswordValidator.isValidPwd("asbide"));
+    public void minimalLenghtButNoDigitPasswordTest() {
+        assertFalse(PasswordValidator.isValidPwd("asbide"));
+    }
+
+    @Test
+    public void passwordAsDigitAndCharButNoMinimalLenght() {
+        assertFalse(PasswordValidator.isValidPwd("a4de"));
     }
 
     @Test
     public void passwordAsDigitTest() {
         assertTrue(PasswordValidator.isValidPwd("as1ide"));
+    }
+
+    @Test
+    public void passwordAsNoDigitTest() {
+        assertFalse(PasswordValidator.isValidPwd("assside"));
+    }
+
+    @Test
+    public void passwordAsCharTest() {
+        assertTrue(PasswordValidator.isValidPwd("12345d6"));
+    }
+
+    @Test
+    public void passwordAsNoCharTest() {
+        assertFalse(PasswordValidator.isValidPwd("12345856"));
     }
 }
